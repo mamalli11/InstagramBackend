9b2a42ab6600b3d34a80cc4d8fa7d95694ed491f
Mohammad Javad <iman7260@gmail.com>
2024-04-06 01:42:24 +0330

    install cookie-parser and use

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app/app.module";
 import { SwaggerConfigInit } from "./config/swagger.config";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	SwaggerConfigInit(app);
+	app.use(cookieParser(process.env.COOKIE_SECRET))
 	const { PORT } = process.env;
 	await app.listen(PORT, () => {
 		console.log(`http://localhost:${PORT} ✅`);

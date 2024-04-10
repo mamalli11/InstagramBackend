31cd2ee42bf1ae59fa896654d977d848db8d022f
Mohammad Javad <iman7260@gmail.com>
2024-04-11 00:59:15 +0330

    Added Static Folder

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./modules/app/app.module";
 import { SwaggerConfigInit } from "./config/swagger.config";
-import * as cookieParser from "cookie-parser";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	SwaggerConfigInit(app);
+	app.useStaticAssets("public");
+	app.useGlobalPipes(new ValidationPipe());
 	app.use(cookieParser(process.env.COOKIE_SECRET));
 	const { PORT } = process.env;
 	await app.listen(PORT, "0.0.0.0", () => {

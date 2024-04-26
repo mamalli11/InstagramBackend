0ebf0459b97879e26ee8d71ace14239e931c4757
Mohammad Javad <iman7260@gmail.com>
2024-04-26 16:36:34 +0330

    Add BlockEntity and FollowRequestEntity to user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,18 +1,27 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { UserService } from "./user.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "../auth/auth.module";
 import { OtpEntity } from "./entities/otp.entity";
 import { UserController } from "./user.controller";
 import { UserEntity } from "./entities/user.entity";
+import { BlockEntity } from "./entities/block.entity";
 import { FollowEntity } from "./entities/follow.entity";
 import { ProfileEntity } from "./entities/profile.entity";
+import { FollowRequestEntity } from "./entities/follow-requst.entity";
 
 @Module({
 	imports: [
 		AuthModule,
-		TypeOrmModule.forFeature([UserEntity, ProfileEntity, OtpEntity, FollowEntity]),
+		TypeOrmModule.forFeature([
+			OtpEntity,
+			UserEntity,
+			BlockEntity,
+			FollowEntity,
+			ProfileEntity,
+			FollowRequestEntity,
+		]),
 	],
 	controllers: [UserController],
 	providers: [UserService],
